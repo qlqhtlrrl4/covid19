9a505c7882c6d3165b171716317fc6b4548e6a2c
qlqhtlrrl4 <qlqhtlrrl45@naver.com>
2021-10-01 19:00:09 +0900

    domain package entity add // package kr.co.soldesk.data add

---
+++ src/main/java/kr/co/soldesk/domain/CovidData.java
@@ -0,0 +1,40 @@
+package kr.co.soldesk.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+import lombok.Data;
+
+@Data
+@Entity
+@TableGenerator(
+		name = "covid_seq_generator",
+		table = "covidhospital_sequence",
+		pkColumnValue = "covid_seq",
+		valueColumnName = "next_val",
+		allocationSize = 1)
+@Table(name="covidhospital")
+public class CovidData {
+	
+	@GeneratedValue(strategy=GenerationType.TABLE, generator = "covid_seq_generator")
+	@Id
+	@Column(name="covid_id", nullable=false , updatable=false)
+	private int id;
+	
+	private String dywk;
+	private String endTm;
+	private String hldyYn;
+	private String lunchEndTm;
+	private String lunchSttTm;
+	private String orgTlno;
+	private String orgZipaddr;
+	private String orgcd;
+	private String orgnm;
+	private String slrYmd;
+	private String sttTm;
+}
